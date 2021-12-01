47b7810ed3a7cccb9e7218a3dacb4063d8570d7a
aam198 <aam198@psu.edu>
2021-12-01 15:06:34 -0500

    Toast add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import MusicList from "./components/MusicList";
 import Header from "./components/Header";
+import toast, { Toaster } from "react-hot-toast";
+import { MdOutlineClose } from "react-icons/md";
+
 
 import './styles/App.css';
 
+const notify = () =>
+  toast((t) => (
+      <div
+        className="notificationWrapper">
+        <div className="iconWrapper">
+        </div>
+        <div className="contentWrapper">
+          <h1>New version available</h1>
+          <p>
+            An improved version of VESSEL is now available, refresh to update.
+          </p>
+        </div>
+        <div className="closeIcon" onClick={() => toast.dismiss(t.id)}>
+          <MdOutlineClose />
+        </div>
+      </div>
+    ),
+    { id: "unique-notification", position: "bottom-center" }
+  );
+
 function App() {
   return (
-
+    
    <div className="container mt-5 mb-3">
+     <div>
+        <button onClick={notify}>Notify</button>
+        <Toaster /> 
+      </div>
+
      <Header />
      <MusicList />
-   </div>
+  
+      
+    </div>
   );
 }
 
